23614548abace2550553b32bda7de3c0580b4214
jongatwork <jon@7ontheleft.net>
2021-01-26 14:52:15 +0000

    -> adding unit test to test the downloading of the data from birthday repo. this test does not yet work. comment says why

---
+++ app/src/androidTest/java/net/sevenontheleft/birthday/BirthdayRepoTest.kt
@@ -0,0 +1,30 @@
+package net.sevenontheleft.birthday
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import net.sevenontheleft.birthday.engine.BirthdayRepo
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BirthdayRepoTest {
+
+    private lateinit var birthdayRepo: BirthdayRepo
+
+    @Before
+    fun start() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        birthdayRepo = BirthdayRepo(context)
+    }
+
+    @Test
+    // this test does not yet work because I cannot invok observeForever on a background thread
+    fun downloadBirthdays() {
+        val birthdayList = birthdayRepo.getBirthdays()
+        birthdayRepo.getBirthdays().observeForever {
+            val dd = 34
+            val ee = 33
+        }
+    }
+}
